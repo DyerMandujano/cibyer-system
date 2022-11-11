913aab6f9fcda98a9d1648bb9850925bb22d07aa
GianJEV <gianesven@hotmail.com>
2022-11-11 17:53:22 -0500

    Entidades Categoria y Producto finalizadas

--- src/main/java/pe/com/cybersystem/entity/CategoriaEntity.java
+++ src/main/java/pe/com/cybersystem/entity/CategoriaEntity.java
@@ -1,5 +1,36 @@
 package pe.com.cybersystem.entity;
 
-public class CategoriaEntity {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "CategoriaEntity")
+@Table(name = "categoria")
+public class CategoriaEntity implements Serializable{
+
+	public static final long serialVersionUID = 1L; 
+	
+	@Id
+	@Column(name = "IdCategoria")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idcat;
+	@Column(name = "NombreCategoria")
+	private String nombrecat;
+	@Column(name = "Estado")
+	private boolean estadocat;
+	
 }
--- src/main/java/pe/com/cybersystem/entity/ProductoEntity.java
+++ src/main/java/pe/com/cybersystem/entity/ProductoEntity.java
@@ -1,5 +1,50 @@
 package pe.com.cybersystem.entity;
 
-public class ProductoEntity {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "ProductoEntity")
+@Table(name = "producto")
+public class ProductoEntity implements Serializable{
+
+	public static final long serialVersionUID = 1L; 
+	
+	@Id
+	@Column(name = "IdProducto ")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idprod;
+	@Column(name = "NombreProducto")
+	private String nombreprod;
+	@Column(name = "Precio")
+	private double precioprod;
+	@Column(name = "Stock")
+	private int stockprod;
+	@Column(name = "Descripcion")
+	private String descripcionprod;
+	@Column(name = "Estado")
+	private boolean estadoprod;
+	@Column(name = "Marca")
+	private String marcaprod;
+	
+	@ManyToOne
+	@JoinColumn(name = "IdCategoria")
+	private CategoriaEntity categoria;
+	
 }
